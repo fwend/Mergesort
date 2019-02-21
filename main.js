93b197f12d39d8f00e4dccc9deee237bf4d30d5c
Jos van Uden <josvanuden@gmail.com>
2019-02-21 02:46:41 +0100

    move nested function out

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
 const a = [2,1,8,4,9,6,7,3,5,0];
 
-const mergeSort = (input) => {
-    const merge = (left, right) => {
+const merge = (left, right) => {
 
-        let result = [];
-        let indexLeft = 0;
-        let indexRight = 0;
+    let result = [];
+    let indexLeft = 0;
+    let indexRight = 0;
 
-        while (indexLeft < left.length && indexRight < right.length) {
-            if (left[indexLeft] < right[indexRight]) {
-                result.push(left[indexLeft]);
-                indexLeft++;
-            } else {
-                result.push(right[indexRight]);
-                indexRight++;
-            }
+    while (indexLeft < left.length && indexRight < right.length) {
+        if (left[indexLeft] < right[indexRight]) {
+            result.push(left[indexLeft]);
+            indexLeft++;
+        } else {
+            result.push(right[indexRight]);
+            indexRight++;
         }
-
-        if (indexRight < right.length) {
-            result = result.concat(right.slice(indexRight));
-            
-        } else if (indexLeft < left.length) {
-            result = result.concat(left.slice(indexLeft));
-        }   
-
-        return result;
     }
-  
+
+    if (indexRight < right.length) {
+        result = result.concat(right.slice(indexRight));
+        
+    } else if (indexLeft < left.length) {
+        result = result.concat(left.slice(indexLeft));
+    }   
+
+    return result;
+}
+
+const mergeSort = (input) => {
+ 
     if (input.length < 2) {
         return input;
     }
